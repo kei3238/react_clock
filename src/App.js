b245f25d1f45c18fc6adf852469a517f5485abbf
Yusuke Kikuchi <yusuke.k11@gmail.com>
2018-04-10 23:08:48 +0900

    manage current time as state

--- src/App.js
+++ src/App.js
@@ -7,21 +7,45 @@ function parseTime(datetime=new Date()){
     second: datetime.getSeconds()
   };
 }
+
 function zeroPadding(number, length=2) {
   return (Array(length).join("0") + number).slice(-length);
 }
 
 class App extends Component {
+
+  constructor(props){
+    super(props);
+    
+    // initial state
+    const datetime = parseTime();
+    this.state = {
+      // '...' expands the specified object 
+      ...datetime
+    };
+    
+    // bind event handler
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  // update state when clicked
+  handleClick(){
+    const datetime = parseTime();
+    this.setState({
+      ...datetime
+    });
+  }
+
   render() {
 
-    const current_time = parseTime();
-    const hour = zeroPadding(current_time.hour);
-    const minute = zeroPadding(current_time.minute);
-    const second = zeroPadding(current_time.second);
+    const hour = zeroPadding(this.state.hour);
+    const minute = zeroPadding(this.state.minute);
+    const second = zeroPadding(this.state.second);
 
     return (
       <div>
         {hour}:{minute}:{second}
+        <button onClick={this.handleClick}>Update</button>
       </div>
     );
   }
